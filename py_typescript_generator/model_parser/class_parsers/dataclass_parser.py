9ffb1b6aa1653e3a8b279cd587525be03f8bc592
Sandro Massa <sandro.massa@giga-storage.com>
2025-02-21 14:26:58 +0100

    [FIX] typing

--- py_typescript_generator/model_parser/class_parsers/dataclass_parser.py
+++ py_typescript_generator/model_parser/class_parsers/dataclass_parser.py
@@ -1,11 +1,12 @@
 from dataclasses import fields
-from typing import Type
+from typing import Optional, Type
 
 from py_typescript_generator.model.py_class import PyClass
 from py_typescript_generator.model.py_field import PyField
 from py_typescript_generator.model_parser.class_parsers.abstract_class_parser import (
     AbstractClassParser,
 )
+from py_typescript_generator.typing_utils.typing_utils import UnknownTypeError, get_type
 
 
 class NotADataclassException(RuntimeError):
@@ -28,6 +29,18 @@ class DataclassParser(AbstractClassParser):
             raise NotADataclassException(cls)
         py_fields = []
         for field in fields(cls):
-            py_fields.append(PyField(name=field.name, type=field.type))
+            typ: Optional[Type]
+            field_type = field.type
+            if isinstance(field_type, str):
+                typ = get_type(field_type)
+                
+                if typ is None:
+                    raise UnknownTypeError(field_type)
+            elif isinstance(field_type, type):
+                typ = field_type
+            else:
+                typ = type(field_type)
+
+            py_fields.append(PyField(name=field.name, type=typ))
 
         return PyClass(name=cls.__name__, type=cls, fields=tuple(py_fields))
--- py_typescript_generator/typing_utils/typing_utils.py
+++ py_typescript_generator/typing_utils/typing_utils.py
@@ -5,6 +5,15 @@ from typing_inspect import get_args  # type: ignore
 T = TypeVar("T")
 
 
+class UnknownTypeError(Exception):
+    def __init__(self, type_name: str, *args: object, **kwargs: dict) -> None:
+        """ Exception raised if a type could not be found from a type name.
+
+        :param type_name: The type name.
+        """
+        super().__init__(f'Unknown type "{type_name}"', *args, **kwargs)
+
+
 def get_wrapped_type_from_optional(cls: Type) -> Type:
     args = get_args(cls)
     return args[0]  # type: ignore
@@ -14,3 +23,25 @@ def safe_unwrap(optional: Optional[T]) -> T:
     if optional is None:
         raise ValueError("Tried to unwrap an optional, but it was None.")
     return optional
+
+
+def get_type(type_name: str) -> Optional[Type]:
+    """ Attempts to get the type from its name.
+
+    :param type_name: The type name.
+
+    :returns: The found type.
+
+    :raises UnknownTypeError: If the type name could not be found in
+        either builtins or globals.
+    """
+    import builtins
+
+    try:
+        return getattr(builtins, type_name)  # type: ignore
+    except AttributeError:
+        try:
+            obj = globals()[type_name]
+        except KeyError:
+            raise UnknownTypeError(type_name)
+        return obj if isinstance(obj, type) else None  # type: ignore
